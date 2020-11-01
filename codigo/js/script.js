a48ddbe507a8f9d2dce2de7229b68fa19ea4be84
Francisco-salaz <l17111924@cdjuarez.tecnm.mx>
2020-10-31 22:19:59 -0600

    Cuento Infantil

---
+++ codigo/js/script.js
@@ -0,0 +1,15 @@
+console.log(5);
+console.log('Hola mundo!'.valueOf(3));
+console.log(234.456);
+console.log(2+3);
+console.log(34-12);
+// Tipos de datos: numericos, String, Boolean, Null, Underfined, Symbol, Object
+// Variables
+var x = 10;
+x = 'Hola';
+let y =  15;
+const vacio = false;
+/* 
+ comentarios 
+*/ 
+
